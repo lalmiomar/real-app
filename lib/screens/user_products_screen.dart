5b029df9ab8e359992912d4a05072c747ad2fd5b
Lalmi Amor Ben El Khettab <amorbenelkhettablalmi@gmail.com>
2021-09-03 23:24:47 +0100

    i make UserProduct Screen

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,19 +1,62 @@
-import 'package:flutter/material.dart';
-import 'package:real_shop/widgets/app_drawer.dart';
-class UserProductScreen extends StatelessWidget {
+import 'dart:ffi';
 
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:real_shop/providers/products.dart';
+import 'package:real_shop/screens/edit_product_screen.dart';
+import 'package:real_shop/screens/product_detail_screen.dart';
+import 'package:real_shop/widgets/app_drawer.dart';
+import 'package:real_shop/widgets/user_product_item.dart';
+
+class UserProductScreen extends StatelessWidget {
   static const routeName = '/user-product';
+
+  Future<Void> _refreshProduct(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false)
+        .fetchAndSetProducts(filterByUser: true);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Order Screen"),
-      ),
-      body: Center(
-        child: Text("Order Screen"),
+        title: Text("Your Products"),
+        actions: [
+          IconButton(
+            onPressed: () =>
+                Navigator.of(context).pushNamed(EditProductScreen.routeName),
+            icon: Icon(Icons.add),
+          ),
+        ],
       ),
       drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: _refreshProduct(context),
+        builder: (ctx, AsyncSnapshot snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? CircularProgressIndicator()
+                : RefreshIndicator(
+                    onRefresh: () => _refreshProduct(context),
+                    child: Consumer<Products>(
+                      builder: (ctx, productData, _) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListView.builder(
+                          itemCount: productData.items.length,
+                          itemBuilder: (_, int index) => Column(
+                            children: [
+                              UserProductItem(
+                                productData.items[index].id,
+                                productData.items[index].title,
+                                productData.items[index].imageUrl,
+                              ),
+                              Divider(),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+      ),
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -19,7 +19,7 @@ class ProductItem extends StatelessWidget {
       child: GridTile(
         child: GestureDetector(
           onTap: () =>
-              Navigator.of(context).pushNamed(ProductDetailScreen.routeName),
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product.id),
           child: Hero(
             tag: product.id,
             child: FadeInImage(
